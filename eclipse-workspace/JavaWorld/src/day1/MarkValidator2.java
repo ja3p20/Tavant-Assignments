c223504bc58754048d27dfbb56621a81db72551f
Jatin <panchaljatin07@gmail.com>
2019-07-12 20:51:50 +0530

    Minor bug fix

--- eclipse-workspace/JavaWorld/src/day1/MarkValidator2.java
+++ eclipse-workspace/JavaWorld/src/day1/MarkValidator2.java
@@ -3,6 +3,44 @@ package day1;
 public class MarkValidator2 {
 	
 	static String markGrade(int mark) {
+		
+		String grade = "";
+		for(int i=1;i<=4;i++) {
+			switch(i) {
+			case 1:
+					if(mark >=60 && mark <74) grade = "C";
+					break;
+					
+			case 2: if(mark >=75 && mark <90) grade = "B";
+					break;
+					
+			case 3: if(mark >=90 && mark <=100) grade = "A";
+					break;
+			case 4:
+					grade = "D";
+					break;
+			}
+		}
+		
+		//System.out.println(grade);
+		return grade;
+		
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(markGrade(45));
+	}
+}
+
+
+//or
+
+/*
+package day1;
+
+public class MarkValidator2 {
+	
+	static String markGrade(int mark) {
 		String grade = "";
 		mark = mark/10;
 		switch(mark) {
@@ -27,6 +65,10 @@ public class MarkValidator2 {
 	}
 	
 	public static void main(String[] args) {
-		System.out.println(markGrade(75));
+		System.out.println(markGrade(90));
 	}
 }
+
+*/
+
+
