5bb3c3be3c11397298f8a6cb3165446839f529e7
Jatin <panchaljatin07@gmail.com>
2019-08-07 18:25:20 +0530

    Minor fix

--- eclipse-workspace/JavaWorld/test/day1/MarkValidatorTest.java
+++ eclipse-workspace/JavaWorld/test/day1/MarkValidatorTest.java
@@ -9,30 +9,27 @@ class MarkValidatorTest {
 	@Test
 	void test() {
 		MarkValidator mvobj = new MarkValidator();
-		mvobj.isPass(90);
-		mvobj.markGrade(90);
 		String expectedgrade = "A";
-		String actualgrade = "A";
+		String actualgrade = mvobj.markGrade(90);
 		String expectedPF = "Pass";
-		String actualPF = "Pass";
+		String actualPF = mvobj.isPass(90);
 		assertEquals(actualgrade,expectedgrade);
 		assertEquals(actualPF,expectedPF);
 	}
 	@Test
 	void test2() {
 		MarkValidator mvobj = new MarkValidator();
-		mvobj.isPass(-123);
+		
 		String expectedgrade = "Please enter valid marks";
-		String actualgrade = "Please enter valid marks";
+		String actualgrade = mvobj.isPass(-123);
 		assertEquals(actualgrade,expectedgrade);
 		
 	}
 	@Test
 	void test3() {
 		MarkValidator mvobj = new MarkValidator();
-		mvobj.isPass(400);
 		String expectedgrade = "Please enter valid marks";
-		String actualgrade = "Please enter valid marks";
+		String actualgrade = mvobj.isPass(400);
 		assertEquals(actualgrade,expectedgrade);
 		
 	}
